5995e8b707c89c69a8ff9bf0594b520a7094233c
Musawer49 <musawerahmedkhan2003@gmail.com>
2026-08-16 22:34:20 +0500

    Added status eature in task

--- To-Do_List.py
+++ To-Do_List.py
@@ -1,4 +1,18 @@
 print("Welcome to To-Do List!")
+
 tasks = []
-tasks.append(input("Add Task: "))
+
+while True:
+    user_input = input("Add a task: ")
+
+    if user_input.lower() == "exit":
+        break
+
+    task = {
+        "task": user_input,
+        "completed": False
+    }
+
+    tasks.append(task)
+
 print(tasks)
