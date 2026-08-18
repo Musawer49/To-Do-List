a5745e3a4614a651d1e9eff7db3baf630e798b3e
Musawer49 <musawerahmedkhan2003@gmail.com>
2026-08-18 23:02:46 +0500

    Build initial To-Do List CLI features

--- To-Do_List.py
+++ To-Do_List.py
@@ -1,19 +1,22 @@
 print("Welcome to To-Do List!")
-
 tasks = []
-
 while True:
-    user_input = input("Add a task: ")
-
-    if user_input.lower() == "exit":
-        break
-    print("Task added successfully!")
-
-    task = {
-        "task": user_input,
-        "status": False
-    }
-
+    print("""
+    1. Add task
+    2. View tasks
+    3. Complete task
+    4. Delete task
+    5. Exit
+    """)
+    user_input = int(input("Choose an option: "))
+    if user_input == 1:
+        task = input("Add a task")
+    elif user_input == 2:
+        print(tasks)
+    task ={
+        "task" : user_input,
+        "status" : "pending",
+        }
     tasks.append(task)
-
-print(tasks)
+for number, task in enumerate(tasks, start=1):
+    print(number,".", task["task"], "-" , task["status"])
