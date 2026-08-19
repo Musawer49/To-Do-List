ac2e2d47135ffac2092a9cf35319fbd1f3ed20be
Musawer49 <musawerahmedkhan2003@gmail.com>
2026-08-19 23:55:58 +0500

    Upgraded to full options

--- To-Do_List.py
+++ To-Do_List.py
@@ -1,5 +1,9 @@
 print("Welcome to To-Do List!")
-tasks = []
+
+tasks = [
+    {"task": "Practice Python", "status": "pending"},
+    {"task": "Reading Book", "status": "pending"}
+]
 while True:
     print("""
     1. Add task
@@ -8,15 +12,24 @@ while True:
     4. Delete task
     5. Exit
     """)
-    user_input = int(input("Choose an option: "))
-    if user_input == 1:
-        task = input("Add a task")
-    elif user_input == 2:
-        print(tasks)
-    task ={
-        "task" : user_input,
-        "status" : "pending",
-        }
-    tasks.append(task)
-for number, task in enumerate(tasks, start=1):
-    print(number,".", task["task"], "-" , task["status"])
+    choice = int(input("Choose an option: "))
+    if choice == 1:
+        task = input("Add a task: ")
+        print("Task added successfully!")
+        tasks.append(task)
+    elif choice == 2:
+        for number, task in enumerate(tasks, start=1):
+            print(number, ".", task["task"], "-", task["status"])
+    elif choice == 3:
+        ask_num = int(input("Which task number you want to complete: "))
+        if ask_num == 1:
+            tasks[0]["status1"] = "completed"
+    elif choice == 4:
+        ask_num = int(input("Which task number do you want to delete: "))
+        if ask_num == 1:
+            del tasks[0]
+    elif choice == 5:
+        print("Good Bye!")
+        break
+    else:
+        print("Invalid numer. Please enter a valid number.")
