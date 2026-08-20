a7bd40e5b2d9fe880b128536334a7fe593272949
Musawer49 <musawerahmedkhan2003@gmail.com>
2026-08-20 23:30:26 +0500

    Add input validation for task operations

--- To-Do_List.py
+++ To-Do_List.py
@@ -1,9 +1,6 @@
 print("Welcome to To-Do List!")
 
-tasks = [
-    {"task": "Practice Python", "status": "pending"},
-    {"task": "Reading Book", "status": "pending"}
-]
+tasks = []
 while True:
     print("""
     1. Add task
@@ -12,22 +9,33 @@ while True:
     4. Delete task
     5. Exit
     """)
-    choice = int(input("Choose an option: "))
+    try:
+        choice = int(input("Choose an option: "))
+    except ValueError:
+        print("Please enter a number")
+        continue
     if choice == 1:
         task = input("Add a task: ")
+        tasks.append({
+            "task" : task,
+            "status" : "pending"
+        })
         print("Task added successfully!")
-        tasks.append(task)
     elif choice == 2:
         for number, task in enumerate(tasks, start=1):
             print(number, ".", task["task"], "-", task["status"])
     elif choice == 3:
-        ask_num = int(input("Which task number you want to complete: "))
-        if ask_num == 1:
-            tasks[0]["status1"] = "completed"
+        try:
+            ask_num = int(input("Which task number you want to complete: "))
+        except ValueError:
+            print("Please enter a number")
+            continue
+        tasks[ask_num - 1]["status"] = "completed"
+        print("Task marked as completed.")
     elif choice == 4:
         ask_num = int(input("Which task number do you want to delete: "))
-        if ask_num == 1:
-            del tasks[0]
+        del tasks[ask_num - 1]
+        print("Task deleted successfully")
     elif choice == 5:
         print("Good Bye!")
         break
